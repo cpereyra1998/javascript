5ddd35ba4521f84e4b177d274bdb5fe5d5450c93
Camila <cpereyra1998@gmail.com>
2022-03-26 20:57:47 -0300

    Desafio Complementario JS 26Mar

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Ejercicio con WHILE */
+
+let capitalArgentina = prompt("Ingresa la capital de Argentina");
+
+    while(capitalArgentina != "Buenos Aires"){
+        alert("Dato erroneo");
+        capitalArgentina = prompt("Ingrese nuevamente la capital de Argentina");
+    }
+
