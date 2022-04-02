cda089d07b782b43f41f6dacc8b59854c73fe883
Camila <cpereyra1998@gmail.com>
2022-04-02 17:12:56 -0300

    Añadir modificaciones

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -5,10 +5,11 @@ let numero1 = parseInt(prompt("Ingrese valor de su primer producto"));
 let numero2 = parseInt(prompt("Ingrese valor de su segundo producto"));
 let numero3 = parseInt(prompt("Ingrese valor de su tercer producto"));
 
-function productos (numero1, numero2, numero3){
-    return numero1 + numero2 + numero3;
+function productos (a, b, c){
+    let suma = a + b + c
+    return suma;
     
 }
 
-let resultado = sumar(numero1, numero2, numero3);
+let resultado = productos(numero1, numero2, numero3);
 console.log("El total de sus productos es", resultado);
