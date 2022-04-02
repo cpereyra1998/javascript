4ac9e82ccd665f660fe325d43c7ade343200f257
Camila <cpereyra1998@gmail.com>
2022-04-02 16:21:54 -0300

    Añado JS y HTML

---
+++ Ejercicio2.js
@@ -0,0 +1,14 @@
+/* Funcion para suma de articulos */
+
+
+let numero1 = parseInt(prompt("Ingrese valor de su primer producto"));
+let numero2 = parseInt(prompt("Ingrese valor de su segundo producto"));
+let numero3 = parseInt(prompt("Ingrese valor de su tercer producto"));
+
+function productos (numero1, numero2, numero3){
+    return numero1 + numero2 + numero3;
+    
+}
+
+let resultado = sumar(numero1, numero2, numero3);
+console.log("El total de sus productos es", resultado);
