9fc25a59c912aea584e35e8b15ca8d29c5d3ff6e
Camila <cpereyra1998@gmail.com>
2022-04-09 16:05:02 -0300

    Arrays

---
+++ Entregables/arrays.js
@@ -0,0 +1,14 @@
+/* Array para cantidad de productos en stock*/
+
+    /* Cantidad de té matcha */
+    const matcha = ["Floral Matcha", "Jasmin Green", "White Nymph"];
+    console.log(green.length);
+    
+        /* Cantidad de té ooblong */
+    const ooblong = ["Black Oolong", "Fairy Pink Ooblong", "Pixie Sunflower"];
+    console.log(ooblong.length);
+    
+        /* Cantida de té total en stock */
+    const stock = matcha.concat(ooblong)
+    console.log(stock.length)
+    
