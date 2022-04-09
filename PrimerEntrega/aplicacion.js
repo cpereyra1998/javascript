e59b3b36b6a69133521ccae6f6817cb790bb96e6
Camila <cpereyra1998@gmail.com>
2022-04-09 16:00:33 -0300

    Añado modificaciones

--- PrimerEntrega/aplicacion.js
+++ PrimerEntrega/aplicacion.js
@@ -1,4 +1,7 @@
-/* Funcion para suma de articulos: iria en el boton buy de la pagina shopping */
+
+                                        /* Pagina SHOPPING */
+
+/* Funcion para suma de articulos */
 
 
 let numero1 = parseInt(prompt("Ingrese valor de su primer producto"));
@@ -14,15 +17,39 @@ function productos (a, b, c){
 let resultado = productos(numero1, numero2, numero3);
 console.log("El total de sus productos es", resultado);
 
-/* Funcion para contraseña: iria en el formulario de la pagina log in*/
+/* Objeto para descripcion de productos */
+
+function Te(nombre, precio, cantidad){
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+}
+
+/* Array para cantidad de productos en stock*/
+
+    /* Cantidad de té matcha */
+const matcha = ["Floral Matcha", "Jasmin Green", "White Nymph"];
+console.log(green.length);
+
+    /* Cantidad de té ooblong */
+const ooblong = ["Black Oolong", "Fairy Pink Ooblong", "Pixie Sunflower"];
+console.log(ooblong.length);
+
+    /* Cantida de té total en stock */
+const stock = matcha.concat(ooblong)
+console.log(stock.length)
+
+
+                                    /* Pagina LOG IN */
+
+/* Funcion para contraseña*/
 
 
 let usuario = prompt("Ingrese su nombre de usuario")
 let contrasena = prompt("Ingrese su contraseña")
 
-    while(contrasena != "contraseñadada"){
+    while(contrasena != "contraseñaEnBase"){
         alert("Dato erroneo");
-        capitalArgentina = prompt("Ingrese nuevamente su contraseña");
+        contrasena = prompt("Ingrese nuevamente su contraseña");
     }
-
 
