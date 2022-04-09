91bbe60caa3c798b728ed2bdcd64282af45a9119
Camila <cpereyra1998@gmail.com>
2022-04-09 16:06:14 -0300

    Mod

--- Entregables/index.js
+++
@@ -1,9 +0,0 @@
-/* Ejercicio con WHILE */
-
-let capitalArgentina = prompt("Ingresa la capital de Argentina");
-
-    while(capitalArgentina != "Buenos Aires"){
-        alert("Dato erroneo");
-        capitalArgentina = prompt("Ingrese nuevamente la capital de Argentina");
-    }
-
