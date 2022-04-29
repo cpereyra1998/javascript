7781f877da4bd095e14bda17b8c2b4d4f5153153
Camila <cpereyra1998@gmail.com>
2022-04-29 20:45:41 -0300

    Shopping libreria

--- eventos.js
+++
@@ -1,8 +0,0 @@
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", validarFormulario);
-
-function validarFormulario(e){
-    e.preventDefault();
-    console.log("Formulario Enviado");
-    alert("Tu mensaje ha sido enviado con exito")    
-}
