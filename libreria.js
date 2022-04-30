120c8bcbe76678205d274de56c0965d0787a3d98
Camila <cpereyra1998@gmail.com>
2022-04-29 23:59:27 -0300

    Commit

---
+++ libreria.js
@@ -0,0 +1,9 @@
+btn.addEventListener('click', () => {
+
+    Swal.fire({
+        title: 'Great!',
+        text: 'Your product has been added',
+        icon: 'success',
+        confirmButtonText: 'Okay'
+    })
+})
